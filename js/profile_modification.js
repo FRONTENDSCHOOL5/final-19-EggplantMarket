ca3684440b642d1422dc63c633125e7bdeba7a77
yejilee0714 <ellie.yejilee@gmail.com>
2023-06-20 14:46:49 +0900

    add : #42 이미지 데이터 불러와서 보여주기, 이미지 수정 후 저장

--- js/profile_modification.js
+++ js/profile_modification.js
@@ -7,79 +7,132 @@ const introInp = document.querySelector('#userinfo');
 
 // 프로필 정보 불러오기
 const url = "https://api.mandarin.weniv.co.kr",
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGZlODMxYjJjYjIwNTY2MzNiNGMwZSIsImV4cCI6MTY5MjM0NDQ0MiwiaWF0IjoxNjg3MTYwNDQyfQ.QvD05ZEqNtVekSCvBtLp8TNg9x-2vfiLtlpxOb8PD2U",
-    userID = "asd_test"
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTEzYWY5YjJjYjIwNTY2MzQ1M2NiNSIsImV4cCI6MTY5MjQyMzQ3NiwiaWF0IjoxNjg3MjM5NDc2fQ.qsUhukxADGoXTjs98N8h-NDWquskgMW3S03OFaEl5-U",
+    userID = "ellie"
 
-async function Load_userinfo(url,token, accountName) { 
-    try{
-    const res = await fetch(url+`/profile/${accountName}`, {
-                    method: "GET",
-                    headers : {
-                        "Authorization" : `Bearer ${token}`
-                    }
-                });
-    const resJson = await res.json();
-    console.log(resJson);
-    return resJson
-} catch(err){
-    console.error(err);
-}}
+async function Load_userinfo(url, token, accountName) {
+    try {
+        const res = await fetch(url + `/profile/${accountName}`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        const resJson = await res.json();
+        console.log(resJson);
+        return resJson
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 // 프로필 정보 저장
 async function saveProfile(url, token) {
-   // 수정된 프로필 정보 가져오기
-    const reqPath = "/user"
-  
+    // 수정된 프로필 정보 가져오기
+    const reqPath = "/user";
+
     // 이미지 넣기
-    const fileName = await postImg()
+    const fileName = await postImg();
+    const imageURL = 'https://mandarin.api.weniv.co.kr/' + fileName;
 
     const data = {
-      "user": {
-        "username": userNameInp.value,
-        "accountname": acNameInp.value,
-        "intro": introInp.value,
-        // "image": imgInp.src
-        "image": 'https://mandarin.api.weniv.co.kr/' + fileName
-      }
-    }
-  
+        "user": {
+            "username": userNameInp.value,
+            "accountname": acNameInp.value,
+            "intro": introInp.value,
+            "image": imageURL,
+        }
+    };
+
     try {
-      const res = await fetch(url + reqPath, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-        body: JSON.stringify(data)
-      });
-  
-      const json = await res.json();
-      console.log(json);
-      // 성공적으로 저장되었을 때 추가적인 처리 가능
+        const currentImageSrc = document.querySelector('.img-cover img').src;
+
+        // 이미지 데이터 저장할 변수 선언
+        let updatedData;
+
+        // 현재 이미지 소스와 다른 경우 데이터의 이미지 URL을 업데이트
+        if (currentImageSrc !== imageURL) {
+            updatedData = { ...data };
+            updatedData.user.image = currentImageSrc;
+        }
+
+        const res = await fetch(url + reqPath, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(updatedData || data)
+        });
+
+        const json = await res.json();
+        console.log(json);
+        // 성공적으로 저장되었을 때 추가적인 처리 가능
     } catch (error) {
-      console.error(error);
-      // 저장 실패 시 에러 처리
+        console.error(error);
+        // 저장 실패 시 에러 처리
     }
-  }
+}
+
+
+async function postImg() {
+    const file = imgInp.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const reqPath = "/image/uploadfile";
+    if (document.querySelector('#btn-upload').files[0]) {
+        formData.append("image", document.querySelector('#btn-upload').files[0])
+        const res = await fetch("https://api.mandarin.weniv.co.kr" + reqPath, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "multipart/form-data"
+            },
+            body: formData
+        });
+        const json = await res.json();
+
+        return json.filename;
+    }
+}
+
+imgInp.addEventListener('change', async (e) => {
+    const formData = new FormData();
+    const imageFile = e.target.files[0];
+    formData.append("image", imageFile);
+    const res = await fetch("https://api.mandarin.weniv.co.kr/image/uploadfile", {
+        method: "POST",
+        body: formData
+    });
+    const json = await res.json();
+    const imageURL = "https://api.mandarin.weniv.co.kr/" + json.filename;
+
+    // 보여지는 이미지 업데이트
+    const imageInput = document.querySelector('.img-cover img');
+    imageInput.src = imageURL;
+});
 
 // 이메일 비밀번호 입력 후 포커스 잃으면 형식 및 유효성 검사
 let validAccountName = false;
 let validUserName = false;
 let validInfo = false;
+let validImage = false;
 
 inpsProfile.forEach(item => {
     item.addEventListener('change', async () => {
         await validateProfile(item);
 
         // 사용자이름 길이 맞고 && 계정ID 형식 맞고 사용가능한 ID면 => 버튼 활성화
-        if (validAccountName || validUserName || validInfo) {
-            console.log('다 통과했는디')
+        // info 혹은 image만 변경해도 저장 가능하도록
+        if (validAccountName || validUserName || validInfo || validImage) {
+            console.log('다 통과했는디');
             submitButton.disabled = false;
         } else {
             submitButton.disabled = true;
         }
-    })
-})
+    });
+});
 
 // 수정 시 사용자 이름과 계정 ID 설정
 async function validateProfile(target) {
@@ -87,10 +140,10 @@ async function validateProfile(target) {
     // 사용자이름 validation
     if (target.id === 'username') {
         if (!target.validity.tooShort && !target.validity.tooLong) {
-            document.querySelector(`.warning-msg-username`).style.display = 'none'
-            validUserName = true
+            document.querySelector(`.warning-msg-username`).style.display = 'none';
+            validUserName = true;
         } else {
-            document.querySelector(`.warning-msg-username`).style.display = 'block'
+            document.querySelector(`.warning-msg-username`).style.display = 'block';
             validUserName = false;
         }
     }
@@ -98,9 +151,9 @@ async function validateProfile(target) {
     // 계정id validation
     if (target.id === 'userid') {
         // 1. pattern 검사
-        console.log(target.validity.patternMismatch)
+        console.log(target.validity.patternMismatch);
         if (target.validity.patternMismatch) {
-            document.querySelector('.warning-msg-userid').textContent = '*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.'
+            document.querySelector('.warning-msg-userid').textContent = '*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.';
             document.querySelector('.warning-msg-userid').style.display = 'block';
         } else {
             // 2. 계정 중복 검사
@@ -121,14 +174,18 @@ async function validateProfile(target) {
         validUserName = true;
     }
 
+    // 이미지 validation
+    if (target.id === 'btn-upload'){
+        validImage = true;
+    }
 }
 
 // 다음버튼 누르면 프로필설정 폼으로 변경
 submitButton.addEventListener('click', async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     await saveProfile(url, token);
-    console.log('프로필 수정 완료')
-})
+    console.log('프로필 수정 완료');
+});
 
 
 // api 연동 
@@ -139,92 +196,29 @@ async function validateUserId() {
         "user": {
             "accountname": acNameInp.value
         }
-    }
+    };
     const res = await fetch(url + reqPath, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(accountNameData)
-    })
+    });
     const json = await res.json();
     return json;
 }
 
-// async function SubmitJoinForm() {
-//     const url = "https://api.mandarin.weniv.co.kr";
-//     const reqPath = "/user"
-
-//     // 이미지 넣기
-//     const fileName = await postImg()
-
-//     const data = {
-//         "user": {
-//             "username": userNameInp.value,
-//             "email": email.value,
-//             "password": pw.value,
-//             "accountname": acNameInp.value,
-//             "intro": introInp.value,
-//             "image": 'https://mandarin.api.weniv.co.kr/' + fileName
-//         }
-//     }
-
-//     const res = await fetch(url + reqPath, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify(data)
-//     })
-
-//     const json = await res.json();
-//     return json;
-// }
-  
-async function postImg() {
-    const formData = new FormData();
-    const reqPath = "/image/uploadfile";
-    if (document.querySelector('#btn-upload').files[0]) {
-        formData.append("image", document.querySelector('#btn-upload').files[0])
-        const res = await fetch("https://api.mandarin.weniv.co.kr" + reqPath, {
-            method: "POST",
-            body: formData
-        });
-        const json = await res.json();
-
-        return json.filename;
-    } else {
-        return "1687141773353.png";
-        // 기본 이미지
-    }
-}
-
-// (function () {
-//     const uploadInp = document.querySelector('#btn-upload')
-//     console.log(uploadInp.files);
-
-//     uploadInp.addEventListener('change', (e) => readURL(e.target));
-
-//     function readURL(input) {
-//         if (input.files && input.files[0]) {
-//             var reader = new FileReader();
-//             reader.onload = function (e) {
-//                 document.querySelector('.img-cover img').src = e.target.result;
-//             };
-//             reader.readAsDataURL(input.files[0]);
-//         } else {
-//             document.querySelector('.img-cover img').src = "../assets/basic-profile-img.png";
-//         }
-//     }
-// })()
-
 function introUpdate(profileData) {
-    // const imgCover = document.querySelector('.img-cover img');
+    const imageInput = document.querySelector('.img-cover img');
     const userNameInput = document.querySelector('#username');
     const accountNameInput = document.querySelector('#userid');
     const introInput = document.querySelector('#userinfo');
 
-    // imgCover.src = profileData.image;
+    // 프로필 이미지 보여주기
+    const imageSrc = profileData.image;
+    if(imageSrc) {
+        imageInput.src = imageSrc;
+    }
     userNameInput.value = profileData.username;
     accountNameInput.value = profileData.accountname;
     introInput.value = profileData.intro;
@@ -232,7 +226,7 @@ function introUpdate(profileData) {
 
 async function run(url, token, accountName) {
     const profileData = await Load_userinfo(url, token, accountName);
-    await introUpdate(profileData.profile);
+    introUpdate(profileData.profile);
 }
 
 run(url, token, userID);

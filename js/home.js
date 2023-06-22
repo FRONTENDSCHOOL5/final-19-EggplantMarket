637c50826bffb560d5bd141b3d640dec5040fd51
jiwon.An <wldnjs7152@naver.com>
2023-06-22 17:13:17 +0900

    #49 refactor : 코드수정(쿼리스트링, 좋아요 이미지 변경)

--- js/home.js
+++ js/home.js
@@ -37,17 +37,17 @@ async function postFeed() {
                         <h2 class="a11y-hidden">사진과 글을 함께 올리는 게시물</h2>
                         <section class="user-follow">
                             <h3 class="a11y-hidden">유저정보</h3>
-                            <a class="profile-img img-cover" href="./profile_info.html" tabindex="1">
+                            <a class="profile-img img-cover" href="./profile_info.html?accountName=${resJson.posts[i].author.accountname}" tabindex="1">
                                 <img src="${resJson.posts[i].author.image}" alt="프로필사진">
                             </a>
-                            <a class="user-info" href="./profile_info.html" tabindex="1">
+                            <a class="user-info" href="./profile_info.html?accountName=${resJson.posts[i].author.accountname}" tabindex="1">
                                 <p class="user-name">${resJson.posts[i].author.username}</p>
                                 <p class="user-id">${resJson.posts[i].author.accountname}</p>
                             </a>
                         </section>
                         <section class="post-edit">
                             <h4 class="a11y-hidden">게시물의 사진과 내용</h4>
-                            <a href="./post_detail.html" tabindex="1">
+                            <a href="./post_detail.html?accountName=${resJson.posts[i].author.accountname}" tabindex="1">
                                 <p class="post-text">${resJson.posts[i].content}</p>
                                     <div class="img-cover">
                                         <img class="post-img" src="${resJson.posts[i].image}" alt="게시물 사진">
@@ -55,9 +55,11 @@ async function postFeed() {
                                 </a>
                                 <div class="post-icon">
                                     <button class="btn-like">
-                                        <img src="../assets/icon/icon-heart.svg" alt="좋아요 버튼"><span class="cnt">${resJson.posts[i].heartCount}</span>
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M16.9202 4.01346C16.5204 3.60579 16.0456 3.28239 15.5231 3.06175C15.0006 2.8411 14.4406 2.72754 13.875 2.72754C13.3094 2.72754 12.7494 2.8411 12.2268 3.06175C11.7043 3.28239 11.2296 3.60579 10.8298 4.01346L9.99997 4.85914L9.17017 4.01346C8.36252 3.19037 7.26713 2.72797 6.12495 2.72797C4.98277 2.72797 3.88737 3.19037 3.07973 4.01346C2.27209 4.83655 1.81836 5.9529 1.81836 7.11693C1.81836 8.28095 2.27209 9.3973 3.07973 10.2204L3.90953 11.0661L9.99997 17.273L16.0904 11.0661L16.9202 10.2204C17.3202 9.81291 17.6376 9.32909 17.8541 8.79659C18.0706 8.26409 18.182 7.69333 18.182 7.11693C18.182 6.54052 18.0706 5.96977 17.8541 5.43726C17.6376 4.90476 17.3202 4.42095 16.9202 4.01346Z" stroke="#767676" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg><span class="cnt">${resJson.posts[i].heartCount}</span>
                                     </button>
-                                    <a class="btn-comment" href="./post_detail.html" tabindex="1">
+                                    <a class="btn-comment" href="./post_detail.html?accountName=${resJson.posts[i].author.accountname}" tabindex="1">
                                         <img src="../assets/icon/icon-message-circle.svg" alt="댓글 버튼"><span class="cnt">${resJson.posts[i].commentCount}</span>
                                     </a>
                                 </div>
@@ -77,25 +79,27 @@ async function postFeed() {
     <h2 class="a11y-hidden">사진과 글을 함께 올리는 게시물</h2>
     <section class="user-follow">
         <h3 class="a11y-hidden">유저정보</h3>
-        <a class="profile-img img-cover" href="./profile_info.html" tabindex="1">
+        <a class="profile-img img-cover" href="./profile_info.html?accountName=${resJson.posts[i].author.accountname}" tabindex="1">
             <img src="${resJson.posts[i].author.image}" alt="프로필사진">
         </a>
-        <a class="user-info" href="./profile_info.html" tabindex="1">
+        <a class="user-info" href="./profile_info.html?accountName=${resJson.posts[i].author.accountname}" tabindex="1">
             <p class="user-name">${resJson.posts[i].author.username}</p>
             <p class="user-id">${resJson.posts[i].author.accountname}</p>
         </a>
     </section>
     <section class="post-edit">
         <h4 class="a11y-hidden">게시물의 사진과 내용</h4>
-        <a href="./post_detail.html" tabindex="1">
+        <a href="./post_detail.html?accountName=${resJson.posts[i].author.accountname}" tabindex="1">
             <p class="post-text">${resJson.posts[i].content}</p>
                 
             </a>
             <div class="post-icon">
                 <button class="btn-like">
-                    <img src="../assets/icon/icon-heart.svg" alt="좋아요 버튼"><span class="cnt">${resJson.posts[i].heartCount}</span>
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M16.9202 4.01346C16.5204 3.60579 16.0456 3.28239 15.5231 3.06175C15.0006 2.8411 14.4406 2.72754 13.875 2.72754C13.3094 2.72754 12.7494 2.8411 12.2268 3.06175C11.7043 3.28239 11.2296 3.60579 10.8298 4.01346L9.99997 4.85914L9.17017 4.01346C8.36252 3.19037 7.26713 2.72797 6.12495 2.72797C4.98277 2.72797 3.88737 3.19037 3.07973 4.01346C2.27209 4.83655 1.81836 5.9529 1.81836 7.11693C1.81836 8.28095 2.27209 9.3973 3.07973 10.2204L3.90953 11.0661L9.99997 17.273L16.0904 11.0661L16.9202 10.2204C17.3202 9.81291 17.6376 9.32909 17.8541 8.79659C18.0706 8.26409 18.182 7.69333 18.182 7.11693C18.182 6.54052 18.0706 5.96977 17.8541 5.43726C17.6376 4.90476 17.3202 4.42095 16.9202 4.01346Z" stroke="#767676" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg><span class="cnt">${resJson.posts[i].heartCount}</span>
                 </button>
-                <a class="btn-comment" href="./post_detail.html" tabindex="1">
+                <a class="btn-comment" href="./post_detail.html?accountName=${resJson.posts[i].author.accountname}" tabindex="1">
                     <img src="../assets/icon/icon-message-circle.svg" alt="댓글 버튼"><span class="cnt">${resJson.posts[i].commentCount}</span>
                 </a>
             </div>

315d0d0fbe0727055a0930b5554fb1ceadd53b58
ebokyung <dlqhrud303@naver.com>
2023-06-22 17:07:43 +0900

    #41 add : 팔로워 목록에서 팔로잉/팔로우 버튼 기능 구현(api)

--- js/follower.js
+++ js/follower.js
@@ -14,10 +14,13 @@ const viewMyFollowerList = accountname === myAccountname ? true : false;
     if (viewMyFollowerList) {
         makeMyFollowerList(data);
         // (팔로우(하기) 버튼만 가능, 팔로워를 삭제하는 버튼은 있지만 기능은 없음 disabled 처리)
-        followers.addEventListener('click', (e) => {
+        followers.addEventListener('click', async (e) => {
             if (e.target.classList.contains('btn-follow')) {
                 // 팔로우 기능
-                console.log('팔로우 할거에요')
+                // 클릭한 요소의 사용자계정 
+                const clickedAccount = e.target.closest('li').querySelector('a').href.split('?accountName=')[1];
+                await postFollow(clickedAccount);
+
                 e.target.innerHTML = `삭제<span class="a11y-hidden">하기 버튼</span>`;
                 e.target.disabled = true;
             }
@@ -25,16 +28,21 @@ const viewMyFollowerList = accountname === myAccountname ? true : false;
     } else {
         makeUserFollowerList(data);
         // (팔로우(하기) 버튼, 팔로잉(취소) 버튼)
-        followers.addEventListener('click', (e) => {
+        followers.addEventListener('click', async (e) => {
             if (e.target.classList.contains('btn-follow')) {
+                // 클릭한 요소의 사용자계정 
+                const clickedAccount = e.target.closest('li').querySelector('a').href.split('?accountName=')[1];
+
                 if (e.target.classList.contains('opposite')) { // 현재 '팔로잉 취소하기 버튼'일때 가지고 있는 클래스
                     // 언팔로우 기능
-                    console.log('언팔로우 할거에요')
+                    await deleteFollow(clickedAccount);
+
                     e.target.classList.remove('opposite');
                     e.target.innerHTML = `팔로우<span class="a11y-hidden">하기 버튼</span>`;
                 } else {
                     // 팔로우 기능
-                    console.log('팔로우 할거에요')
+                    await postFollow(clickedAccount);
+
                     e.target.classList.add('opposite');
                     e.target.innerHTML = `팔로잉<span class="a11y-hidden">취소 버튼</span>`;
                 }
@@ -43,6 +51,44 @@ const viewMyFollowerList = accountname === myAccountname ? true : false;
         )
     }
 })()
+
+
+// POST 팔로우
+async function postFollow(accountName) {
+    try {
+        let res = await fetch('https://api.mandarin.weniv.co.kr' + `/profile/${accountName}/follow`, {
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-type": "application/json"
+            }
+        });
+
+        const resJson = await res.json()
+        console.log('팔로우 완료 : ', resJson.profile);
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// DELETE 팔로우
+async function deleteFollow(accountName) {
+    try {
+        let res = await fetch('https://api.mandarin.weniv.co.kr' + `/profile/${accountName}/unfollow`, {
+            method: 'DELETE',
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-type": "application/json"
+            }
+        });
+
+        const resJson = await res.json()
+        console.log('팔로우 취소 완료 : ', resJson.profile);
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 
 // GET 사용자 팔로워 목록 
 async function getFollowerList() {

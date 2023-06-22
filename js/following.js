6ba5eebbe23b907fd30000f047071f4c3ebd121e
ebokyung <dlqhrud303@naver.com>
2023-06-22 17:10:22 +0900

    #41 add : 팔로잉 목록에서 팔로잉/팔로우 버튼 기능 구현(api)

--- js/following.js
+++ js/following.js
@@ -4,20 +4,64 @@ const accountname = new URLSearchParams(location.search).get('accountName'),
 
 const followings = document.querySelector('.follow-list');
 
-followings.addEventListener('click', (e) => {
+followings.addEventListener('click', async (e) => {
     if (e.target.classList.contains('btn-follow')) {
+        // 클릭한 요소의 사용자계정 
+        const clickedAccount = e.target.closest('li').querySelector('a').href.split('?accountName=')[1];
+
         if (e.target.classList.contains('opposite')) {
             // 언팔로우 기능
+            await deleteFollow(clickedAccount);
+
             e.target.classList.remove('opposite');
             e.target.innerHTML = `팔로우<span class="a11y-hidden">하기 버튼</span>`;
         } else {
             // 팔로우 기능
+            await postFollow(clickedAccount);
+
             e.target.classList.add('opposite');
             e.target.innerHTML = `팔로잉<span class="a11y-hidden">취소 버튼</span>`;
         }
     }
 }
 )
+
+
+// POST 팔로우
+async function postFollow(accountName) {
+    try {
+        let res = await fetch('https://api.mandarin.weniv.co.kr' + `/profile/${accountName}/follow`, {
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-type": "application/json"
+            }
+        });
+
+        const resJson = await res.json()
+        console.log('팔로우 완료 : ', resJson.profile);
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// DELETE 팔로우
+async function deleteFollow(accountName) {
+    try {
+        let res = await fetch('https://api.mandarin.weniv.co.kr' + `/profile/${accountName}/unfollow`, {
+            method: 'DELETE',
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-type": "application/json"
+            }
+        });
+
+        const resJson = await res.json()
+        console.log('팔로우 취소 완료 : ', resJson.profile);
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 // GET 사용자 팔로잉 목록
 async function getFollowingList() {

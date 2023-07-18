1ba967356d6f6066c9c9918bb527fead729b7d81
ebokyung <dlqhrud303@naver.com>
2023-07-18 16:38:42 +0900

    #96 follower.js add & refactor : 팔로잉 기능 추가 및 displayFollowerList 함수 하나로 병합

--- js/follower.js
+++ js/follower.js
@@ -1,7 +1,3 @@
-/*
-- 팔로잉(취소) 버튼 기능 추가 예정
-*/
-
 const accountname = new URLSearchParams(location.search).get('accountName'),
     myAccountname = localStorage.getItem('user-accountname'),
     token = localStorage.getItem('user-token');
@@ -14,51 +10,19 @@ const viewMyFollowerList = accountname === myAccountname ? true : false;
 (async function () {
     const getFollowerList = fetchFollowerList(); // 클로저 함수 반환
     const data = await getFollowerList();
-    if (viewMyFollowerList) displayMyFollowerList(data);
-    else displayUserFollowerList(data);
+    displayFollowerList(data);
 
     // 무한 스크롤 
     window.addEventListener("scroll", async () => {
         if (getScrollTop() >= getDocumentHeight() - window.innerHeight) {
-            if (viewMyFollowerList) {
-                displayMyFollowerList(await getFollowerList())
-            } else {
-                displayUserFollowerList(await getFollowerList())
-            }
+            displayFollowerList(await getFollowerList())
         };
     })
 })();
 
-// 내 팔로워 목록 뿌리기 
-// : (팔로우(하기) 버튼만 가능, 팔로워를 삭제하는 버튼은 있지만 기능은 없음 disabled 처리)
-async function displayMyFollowerList(data) {
-    const frag = document.createDocumentFragment();
-
-    data.forEach(user => {
-        const li = document.createElement('li');
-        li.setAttribute('class', 'follow-item');
-        li.innerHTML = `<a class="user-img img-cover" href="./profile_info.html?accountName=${user.accountname}">
-        <span class="a11y-hidden">${user.username}의 프로필 보기</span>
-        <img src=${checkImageUrl(user.image, 'profile')} alt="">
-        </a>
-        <div class="user-info">
-            <strong class="user-name">
-                <a href="./profile_info.html?accountName=${user.accountname}">${user.username}<span class="a11y-hidden">의 프로필 보기</span></a>
-            </strong>
-            <p class="user-intro ellipsis">${user.intro}</p>
-        </div>
-        ${user.isfollow ? '<button class="btn-follow opposite">팔로잉<span class="a11y-hidden">취소</span></button>' : `<button class="btn-follow">팔로우<span class="a11y-hidden">하기</span></button>`}`
-        // <button class="btn-follow-cancle" disabled>삭제<span class="a11y-hidden">하기</span></button>
-
-        frag.append(li);
-    })
-
-    $followers.append(frag);
-}
-
-// 다른 사용자 팔로워 목록 뿌리기 
-// : (팔로우(하기) 버튼, 팔로잉(취소) 버튼)
-async function displayUserFollowerList(data) {
+// 팔로워 목록 뿌리기 
+// : 내 팔로워 목록에서 팔로워를 삭제하는 버튼은 있지만 기능은 없음 (disabled 처리)
+async function displayFollowerList(data) {
     const frag = document.createDocumentFragment();
 
     data.forEach(user => {
@@ -73,8 +37,14 @@ async function displayUserFollowerList(data) {
             <a href="./profile_info.html?accountName=${user.accountname}">${user.username}<span class="a11y-hidden">의 프로필 보기</span></a>
         </strong>
         <p class="user-intro ellipsis">${user.intro}</p>
-    </div>
-    ${user.accountname !== myAccountname ? (user.isfollow ? `<button class="btn-follow opposite">팔로잉<span class="a11y-hidden">취소</span></button>` : `<button class="btn-follow">팔로우<span class="a11y-hidden">하기</span></button>`) : (``)}`
+    </div>`
+
+        const btn = user.accountname !== myAccountname ? (user.isfollow ? `<button class="btn-follow opposite">팔로잉<span class="a11y-hidden">취소</span></button>` : `<button class="btn-follow">팔로우<span class="a11y-hidden">하기</span></button>`) : (``)
+        li.insertAdjacentHTML('beforeend', btn);
+
+        if (viewMyFollowerList) {
+            li.insertAdjacentHTML('beforeend', `<button class="btn-follow-cancle" disabled>삭제<span class="a11y-hidden">하기</span></button>`);
+        }
 
         frag.append(li);
     })
@@ -82,44 +52,28 @@ async function displayUserFollowerList(data) {
     $followers.append(frag);
 }
 
-// 버튼 이벤트
-if (viewMyFollowerList) {
-    // (팔로우(하기) 버튼만 가능, 팔로워를 삭제하는 버튼은 있지만 기능은 없음 disabled 처리)
-    $followers.addEventListener('click', async (e) => {
-        if (e.target.classList.contains('btn-follow')) {
+// (팔로우(하기) 버튼, 팔로잉(취소) 버튼) 이벤트 위임
+$followers.addEventListener('click', async (e) => {
+    if (e.target.classList.contains('btn-follow')) {
+        // 클릭한 요소의 사용자계정 
+        const clickedAccount = e.target.closest('li').querySelector('a').href.split('?accountName=')[1];
+
+        if (e.target.classList.contains('opposite')) { // 현재 '팔로잉 취소하기 버튼'일때 가지고 있는 클래스
+            // 언팔로우 기능
+            await deleteFollow(clickedAccount);
+
+            e.target.classList.remove('opposite');
+            e.target.innerHTML = `팔로우<span class="a11y-hidden">하기</span>`;
+        } else {
             // 팔로우 기능
-            // 클릭한 요소의 사용자계정 
-            const clickedAccount = e.target.closest('li').querySelector('a').href.split('?accountName=')[1];
             await postFollow(clickedAccount);
 
-            e.target.innerHTML = `삭제<span class="a11y-hidden">하기</span>`;
-            e.target.disabled = true;
-        }
-    })
-} else {
-    // (팔로우(하기) 버튼, 팔로잉(취소) 버튼)
-    $followers.addEventListener('click', async (e) => {
-        if (e.target.classList.contains('btn-follow')) {
-            // 클릭한 요소의 사용자계정 
-            const clickedAccount = e.target.closest('li').querySelector('a').href.split('?accountName=')[1];
-
-            if (e.target.classList.contains('opposite')) { // 현재 '팔로잉 취소하기 버튼'일때 가지고 있는 클래스
-                // 언팔로우 기능
-                await deleteFollow(clickedAccount);
-
-                e.target.classList.remove('opposite');
-                e.target.innerHTML = `팔로우<span class="a11y-hidden">하기</span>`;
-            } else {
-                // 팔로우 기능
-                await postFollow(clickedAccount);
-
-                e.target.classList.add('opposite');
-                e.target.innerHTML = `팔로잉<span class="a11y-hidden">취소</span>`;
-            }
+            e.target.classList.add('opposite');
+            e.target.innerHTML = `팔로잉<span class="a11y-hidden">취소</span>`;
         }
     }
-    )
 }
+)
 
 // --- API 함수들 ---
 
